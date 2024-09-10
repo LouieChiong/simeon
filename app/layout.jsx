7dc74d5772e38a7ed61d931a6c2ee842591244ca
louie chiong <louiechiong@gmail.com>
2024-09-10 23:45:02 +0800

    navigation branding

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,13 @@
 import localFont from "next/font/local";
-import './globals.css'
+import './globals.scss'
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+  src: "../src/fonts/GeistVF.woff",
   variable: "--font-geist-sans",
   weight: "100 900",
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+  src: "../src/fonts/GeistMonoVF.woff",
   variable: "--font-geist-mono",
   weight: "100 900",
 });
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
 import Navigator from "./components/navigator";
-import './globals.css'
+import './globals.scss'
 
 export default function Home() {
   return (
-    <div className="bg-red-50 w-full mx-auto items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-         <Navigator />
+    <div className="bg-red-50 w-full mx-auto items-center min-h-screen">
+        <Navigator />
     </div>
   );
 }
