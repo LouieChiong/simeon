e5133f6879a322736cd7453ab12d27919cf854bd
louie chiong <louiechiong@gmail.com>
2024-12-20 22:35:52 +0800

    update favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        {/* <link rel="icon" href="/images/Logo.png" /> */}
+        <link rel="icon" href="/images/favicon.ico" />
         <title>{metadata.title}</title>
         <meta name="title" content={metadata.title} />
         <meta name="description" content={metadata.description} />
